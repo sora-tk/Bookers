26f0f3e3cda198615c521785f81eb196eee10e34
sora-tk <139532574+sora-tk@users.noreply.github.com>
2023-10-12 03:21:44 +0000

    [Finish]Bookers

---
+++ test/controllers/books_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class BooksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get books_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get books_show_url
+    assert_response :success
+  end
+
+  test "should get editedit" do
+    get books_editedit_url
+    assert_response :success
+  end
+end
